c695b743d85afdf7f8e45b15d33ca69a36e2fa0d
Kyunghyun Koo <kyunghyunkoo@MacBook-Pro.local>
2022-05-31 16:08:27 +0900

    [ADD]Page & Nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,12 @@
 import React from 'react';
 import './App.css';
+import Nav from './components/Nav';
 import Login from './pages/Login';
 
 function App() {
   return (
     <div className="App">
-      <nav className="navbar navbar-expand-md navbar-dark bg-dark mb-4">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#">Home</a>
-          <div>
-            <ul className="navbar-nav me-auto mb-2 mb-md-0">
-              <li className="nav-item">
-                <a className="nav-link active" href="#">Login</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link active" href="#">Register</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
+      <Nav/>
       <main className="form-signin">
         <Login/>
       </main>
